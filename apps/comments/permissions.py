c4f4c2cbb1fc925ed10c44898cc772449c61f0a1
Romanumi <romankirpichikov@gmail.com>
2025-12-11 16:11:58 +0300

    add comments app and admin.py for main

---
+++ apps/comments/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Разрешение, позволяющее редактировать комментарий только автору
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Разрешение на чтение для всех
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Разрешение на запись только для автора
+        return obj.author == request.user
