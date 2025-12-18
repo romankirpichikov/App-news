67b0903ce7275cd64b500825d494c106f64bac8d
Romanumi <romankirpichikov@gmail.com>
2025-12-18 15:32:23 +0300

    finish subscribe

--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/posts/', include('apps.main.urls')),
-    path('api/v1/auth/', include('apps.accounts.urls')),
-    path('api/v1/comments/', include('apps.comments.urls')),
-#    path('api/v1/subscribe/', include('apps.subscribe.urls')),
-#    path('api/v1/payment/', include('apps.payment.urls')),
+    path("admin/", admin.site.urls),
+    path("api/v1/posts/", include("apps.main.urls")),
+    path("api/v1/auth/", include("apps.accounts.urls")),
+    path("api/v1/comments/", include("apps.comments.urls")),
+    path("api/v1/subscribe/", include("apps.subscribe.urls")),
+    #    path('api/v1/payment/', include('apps.payment.urls')),
 ]
 
 if settings.DEBUG:
